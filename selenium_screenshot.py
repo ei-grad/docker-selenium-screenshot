7beaaaca035c5afd9df2dc60a52ad6442fb3ddbd
Andrew Grigorev <andrew@ei-grad.ru>
2016-02-11 21:53:45 +0300

    Use worker pool

--- selenium_screenshot.py
+++ selenium_screenshot.py
@@ -1,4 +1,5 @@
-from threading import Lock
+from threading import RLock, local
+from multiprocessing.pool import ThreadPool
 from os import environ as ENV
 import logging.config
 
@@ -16,26 +17,48 @@ app = Flask(__name__)
 Driver = getattr(webdriver, ENV.get("WEBDRIVER", "Chrome"))
 
 
+class RetryFailed(Exception):
+    pass
+
+
 class Engine():
 
     def __init__(self):
         self.driver = Driver()
-        self.lock = Lock()
+        self.lock = RLock()
 
-    def render(self, url):
+    def render(self, url, retry=0):
+        if retry > 3:
+            raise RetryFailed()
         with self.lock:
-            self.driver.get(url)
-            return self.driver.get_screenshot_as_png()
+            try:
+                self.driver.get(url)
+                return self.driver.get_screenshot_as_png()
+            except:
+                self.driver = Driver()
+                return self.render(url, retry + 1)
 
 
-engine = Engine()
+thread_local = local()
+
+
+def thread_init():
+    thread_local.engine = Engine()
+
+
+pool = ThreadPool(int(ENV.get("SCREENSHOT_WORKERS", 4)),
+                  thread_init)
+
+
+def render(url):
+    return thread_local.engine.render(url)
 
 
 @app.route('/')
 def screenshot():
     url = request.args.get('url')
     logging.info("Got request for url: %s", url)
-    return engine.render(url), 200, {
+    return pool.apply(render, (url,)), 200, {
         'Content-Type': 'image/png',
     }
 
